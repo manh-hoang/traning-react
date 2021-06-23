4b5544daddb2ece6814c99f2f2959013679cd75c
manh-hoang <hhmanh010801@gmail.com>
2021-06-23 20:27:15 +0700

    add react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+function One(){
+  return (
+  
+    <div>
+      <h1>heloo</h1>
+      <h1>heloo</h1>
 
+    </div>
+
+  )
+}
+var Two = function(){
+  return <h1>hello</h1>
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <One/>
+        <Two/>
       </header>
     </div>
   );
